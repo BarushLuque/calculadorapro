c5538884e032a286a1913f717991399bd8b75e48
lenovo <lenovo@10.232.11.73>
2024-05-07 12:44:37 -0700

    Se agrego el codigo faltante

--- src/main/java/CalculadoraPro.java
+++ src/main/java/CalculadoraPro.java
@@ -7,6 +7,7 @@ import java.util.Scanner;
 public class CalculadoraPro {
 
     public static void main(String[] args) {
+        //Hola mi bros
         try {
         Scanner sc = new Scanner(System.in);
         System.out.println("Ingresa el primer digito.");
